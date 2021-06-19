c72ac469684800c63a207da908a0564c4d9efd38
iem-saad <saadmughal96000@gmail.com>
2021-06-20 02:21:39 +0500

    Fixed: Issues with bootsrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,13 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import 'bootstrap'
+import "../stylesheets/application";
+document.addEventListener("turbolinks:load", function() {
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+        $('[data-toggle="popover"]').popover()
+    })
+})
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
@@ -16,3 +22,4 @@ require("channels")
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+import "bootstrap"
